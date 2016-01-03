2e216c8280c02e60b8939dc448d27693b56d382e
panicbit <panicbit.dev@gmail.com>
2016-01-03 06:01:06 +0100

    Implement Debug for StringBuffer

--- src/string_buffer.rs
+++ src/string_buffer.rs
@@ -198,6 +198,12 @@ impl fmt::Display for StringBuffer {
     }
 }
 
+impl fmt::Debug for StringBuffer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "\"{}\"", self)
+    }
+}
+
 impl<'a> Iterator for Chars<'a> {
     type Item = (char, usize);
 
